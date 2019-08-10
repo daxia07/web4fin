809d109b24f870a03fa646c9334211cf133d554e
alienz <limingxia07@gmail.com>
2019-08-10 12:35:13 +1000

    added views to modify posts; fixed misspell delete to detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('post/<int:pk>/', views.PostDeleteView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
