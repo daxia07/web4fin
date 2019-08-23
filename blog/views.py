46304d18822f2b611c0d91ff2570c92483a8987f
alienz <limingxia07@gmail.com>
2019-08-23 14:05:47 +1000

    added responsive condition and fixed foot bar issue

--- blog/views.py
+++ blog/views.py
@@ -67,7 +67,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About Page'})
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 
 def index(request):
